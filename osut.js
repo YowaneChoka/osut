770b61f2fe9aff9e865f9a78041179885af6b7e6
Yowane Choka <cyowane@gmail.com>
2022-05-18 14:46:45 +0800

    Finish OSUT
    Finished simple js working with buttons. Next redesign buttons to images.

--- osut.js
+++ osut.js
@@ -1,15 +1,56 @@
-const computerChoiceDisplay=document.getElementById('computer-choice')
-const myChoiceDisplay=document.getElementById('my-choice')
-const resultDisplay=document.getElementById('result')
+const computerChoiceText = document.querySelector('#computer-choice')
+const myChoiceText = document.querySelector('#my-choice')
+const resultText = document.querySelector('#result')
 
 // select all by class 'button'
-const possibleChoices = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('.game-btn')
 
-let userChoice
+let player, computer, result;
 
-possibleChoices.forEach(possibleChoices=>possibleChoices.addEventListener('click',(e)=>{
-    // e is object of possibleChoices
-    userChoice = e.target.id
+buttons.forEach(button => button.addEventListener("click", () => {
+    //for each button in buttons addEventListener
 
-    myChoiceDisplay.textContent = userChoice
-}))
+    player = button.textContent;
+    computerTurn();
+    computerChoiceText.textContent = `Computer: ${computer}`;
+    myChoiceText.textContent = `Player: ${player}`;
+
+    resultText.textContent = checkWin();
+}));
+
+function computerTurn() {
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    if (randNum == 1) {
+        computer = "ROCK";
+    } else if (randNum == 2) {
+        computer = "PAPER";
+    } else if (randNum == 3) {
+        computer = "SCISSORS";
+    }
+
+}
+
+function checkWin() {
+
+    console.log('checking');
+
+    if (player == computer) {
+        return "Draw!";
+    } else if (player == "ROCK" && computer == "SCISSORS") {
+        return "YOU WIN!";
+    } else if (player == "PAPER" && computer == "ROCK") {
+        return "YOU WIN!";
+    }
+    return "YOU LOSE";
+}
+
+// let userChoice
+
+// possibleChoices.forEach(possibleChoices=>possibleChoices.addEventListener('click',(e)=>{
+//     // e is object of possibleChoices
+//     userChoice = e.target.id
+
+//     myChoiceDisplay.textContent = userChoice
+
+// }))
